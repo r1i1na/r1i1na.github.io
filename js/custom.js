6fc67fb27be7fb4e9d75345646aa008c3e4a1256
Rina <riniwt0721@gmail.com>
2024-11-28 19:05:10 +1100

    Site updated: 2024-11-28 19:05:10

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.body.style.cursor = 'url("/cur/nor.cur"), pointer';
+    document.body.style.cursor = 'url("/cur/nor.cur"), auto';
 
-    document.addEventListener('selectstart', function () {
-        document.body.style.cursor = 'url("/cur/text_select.cur"), text';
-    });
-    document.addEventListener('mouseup', function () {
-        document.body.style.cursor = 'url("/cur/nor.cur"), pointer';
-    });
-    
-    const clickableElements = document.querySelectorAll('a, button, .clickable, .site-page');
-    clickableElements.forEach(element => {
+    document.querySelectorAll('a, button, .clickable').forEach(element => {
         element.addEventListener('mouseenter', function () {
             document.body.style.cursor = 'url("/cur/link_select.cur"), pointer';
         });
-
         element.addEventListener('mouseleave', function () {
-            document.body.style.cursor = 'url("/cur/nor.cur"), pointer';
+            document.body.style.cursor = 'url("/cur/nor.cur"), auto';
+        });
+    });
+
+    document.querySelectorAll('textarea, input, select').forEach(element => {
+        element.addEventListener('mouseenter', function () {
+            document.body.style.cursor = 'url("/cur/text_select.cur"), text';
+        });
+        element.addEventListener('mouseleave', function () {
+            document.body.style.cursor = 'url("/cur/nor.cur"), auto';
         });
     });
 });
